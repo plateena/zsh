a1fa5333787a1fb15daeae83267ed3bc5dcb996d
zainundin <dean.coders@gmail.com>
2023-03-11 23:50:06 +0800

    feat(prompt): aadd git prompt

--- prompt.zsh
+++ prompt.zsh
@@ -3,36 +3,82 @@
 # can checkot color at https://www.tweaking4all.com/software/macosx-software/xterm-color-cheat-sheet/
 black="235"
 white="015"
+# dir_arrow_end_bg="039"
+dir_arrow_end_bg="039"
 
 echo "prompt loaded"
 autoload -Uz vcs_info
-
+#
 zstyle ':vcs_info:git:*' formats '%b '
 
-function git_branch_name() {
-  branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
+# precmd() { vcs_info }
+
+git_branch_name () {
+    branch=$(git branch --show-current)
+    local bg="red"
+    local prefix=$(git rev-parse --show-prefix | sed 's/\/$//')
+    local pr=""
+    local dir=$(git rev-parse --show-toplevel)
+
+    if [[ $(git diff) ]]; then
+        bg="208"
+    else
+        bg="$black"
+    fi
+
+    dir_arrow_end_bg="$bg"
+
+    pr+="%K{$black}%F{green} $dir%K{$bg}%F{$black}%K{$bg}%F{015} $branch %K{$black}%F{$bg}"
+    if [ ! -z "$prefix" ]; then
+        pr+=" %F{green}$prefix %K{039}%F{$black}%F{none}"
+    fi
+
+    echo "$pr"
 }
 
-precmd() { vcs_info }
-precmd() { git_branch_name }
-echo "the branch is $branch"
+is_inside_git ()
+{
+    if git status &>/dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+get_current_dir () {
+    dir="%~"
+    if is_inside_git; then
+        echo $(git rev-parse --show-toplevel)
+    else
+        echo $dir
+    fi
+}
+
+echo $dir_arrow_end_bg
+
 
 setopt PROMPT_SUBST
 
-p=""
-p+="%K{$white}%B %(?.%F{035}√.%F{red}!)%b %K{black}%F{$white}"
-p+="%K{$black}%F{green} %~ %K{039}%F{$black}"
-# p+="%K{039}%F{015} |%(${vcs_info_msg_0_}=='master'.'sdf'.'D')| "
-p+="%K{039}%F{015} $(echo "$branch")"
+precmd() {
+    p=""
+    p+="%K{$white}%B %(?.%F{035}√.%F{red}!)%b %K{black}%F{$white}"
 
-p+="%K{none}%F{039}"
+    if is_inside_git; then
+        p+="$(git_branch_name)"
+    else
+        p+="%K{$black}%F{green} %~ %K{039}%F{$black}"
+        p+="%K{none}%F{039}"
+    fi
 
-PROMPT="$p %# %K{none}%F{none}"
 
-rp=""
-rp+="%b%2~ "
-rp+="%K{none}%F{$white}%B%K{$white}%F{$black} %* "
-# h for history number
-rp+="%K{$white}%F{040}%K{040}%F{$black} %D %h"
-rp+=" %F{016}%B%b"
-RPROMPT="$rp%K{none}%F{none}"
+    PROMPT="$p %# %K{none}%F{039}%K{none}%F{none} "
+
+    rp=""
+    # rp+="%b%2~ "
+    rp+="%K{none}%F{$white}%B%K{$white}%F{$black} %* "
+    # h for history number
+    rp+="%K{$white}%F{040}%K{040}%F{$black} %D %h"
+    rp+=" %F{016}%B%b"
+    rp+="$(lsb_release -a | grep Description | cut -d : -f 2 | xargs echo -n)"
+    RPROMPT="$rp%K{none}%F{none}"
+}
