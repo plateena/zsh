10b80553dbb5d489e00779d7af70a077c277ad2f
zainundin <dean.coders@gmail.com>
2023-03-12 21:57:46 +0800

    feat(keybind): add menu select keybind

--- keybind.zsh
+++ keybind.zsh
@@ -7,4 +7,11 @@ bindkey jk vi-cmd-mode
 source $XDG_CONFIG_HOME/fzf/shell/key-bindings.zsh
 source $XDG_CONFIG_HOME/fzf/shell/completion.zsh
 
-alias sa='source $HOME/.config/zsh/.zshrc; echo "ZSH aliases sourced."'
+zstyle ':completion:*' menu select
+zmodload zsh/complist
+
+# use the vi navigation keys in menu completion
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
