6222c82f7fa49d924a83fb6d505640d4ab209b70
zainundin <plateena711@gmail.com>
2023-03-21 12:45:09 +0800

    style(alias): update git log display

--- alias.zsh
+++ alias.zsh
@@ -75,7 +75,7 @@ set_alias () {
     abbrev-alias gp='git pull origin $(git branch | fzf | awk '\''{print $2}'\'' | xargs echo -e)'
     abbrev-alias gpc='git pull origin $(git branch --show-current | xargs echo -e)'
     # check https://git-scm.com/docs/pretty-formats
-    abbrev-alias glp='git log --pretty=format:"%C(auto) %h %>(9,trunc)%ch %Cblue%cn%C(auto)%d %s"'
+    abbrev-alias glp='git log --pretty=format:"%C(auto) [%h] - %d %s %Cblue%cn%C(auto) %C(cyan)%ch%C(auto)"'
     abbrev-alias gcn='git config user.name '
     abbrev-alias gce='git config user.email '
 
